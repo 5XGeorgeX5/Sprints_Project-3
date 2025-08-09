701cf082b79137b4157b40d0125dbc355cba56e4
kadyy <ahmedkhaled12004@gmail.com>
2025-08-09 08:20:30 +0300

    added Type & Amount fields in the TransactionModel with Enums of the Transaction Type

--- src/main/java/com/team2/bank/Models/TransactionModel.java
+++ src/main/java/com/team2/bank/Models/TransactionModel.java
@@ -1,5 +1,7 @@
 package com.team2.bank.Models;
 
+import com.team2.bank.Enums.TransactionType;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -9,6 +11,13 @@ public class TransactionModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
 
     @ManyToOne()
     @JoinColumn(name = "customer_id")
